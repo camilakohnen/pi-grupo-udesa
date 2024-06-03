d1d5c03648fb2edf2aa400cfb3849b6993f946c4
bernimontero <bmonteromorizzio@udesa.edu.ar>
2024-06-03 12:11:11 -0300

    dividiendo modelos con readme

--- myapp/controllers/indexController.js
+++ myapp/controllers/indexController.js
@@ -1,22 +1,24 @@
-const data = require("../database/models");
+const db = require("../database/models");
 
-db.Productos.findAll()
-.then((results)=>{
-  return res.render("index")
-}).catch((err)=>{
 
-})
 
 let indexController = {
-    index : function(req, res, next) {
-        res.render('index' , {lista: data.productos});
-      },
-    register : function (req, res) {
-        res.render('register');
-    },
-    login : function (req, res) {
-      res.render('login');
-    }
+  index: function (req, res, next) {
+
+    db.Comentario.findAll()
+      .then((results) => {
+        return res.send(results)
+        return res.render("index")
+      }).catch((err) => {
+          return console.log(err);
+      })
+  },
+  register: function (req, res) {
+    res.render('register');
+  },
+  login: function (req, res) {
+    res.render('login');
+  }
 };
 
 module.exports = indexController;
