8777c89f90f91b56f37df3e1818e665e694c87dc
camilakohnen <ckohnen@udesa.edu.ar>
2024-06-10 07:58:57 -0500

    relaciones de modelo usuario

--- myapp/database/models/Usuario.js
+++ myapp/database/models/Usuario.js
@@ -40,5 +40,19 @@ module.exports= function(sequelize, dataTypes){
 
     let Usuario = sequelize.define(alias, cols, config);
 
+    Usuario.associate = function(models){
+        Usuario.hasMany(models.Producto, {
+            as: "producto",
+            foreignKey: "idUsuarios"
+        });
+    }
+
+    Usuario.associate = function(models){
+        Usuario.hasMany(models.Comentario, {
+            as: "comentario",
+            foreignKey: "idUsuario"
+        });
+    }
+
     return Usuario;
 }
